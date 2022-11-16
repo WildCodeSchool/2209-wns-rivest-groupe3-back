4fce666ac8dc42fb50a81adafd1f6ab044101187
Holmes-EH <holmes.samuel@yahoo.fr>
2022-11-16 13:46:39 +0100

    add User entity

---
+++ src/entities/User.ts
@@ -0,0 +1,47 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm'
+import { Field, ObjectType } from 'type-graphql'
+
+@ObjectType()
+@Entity()
+export class User {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Field()
+  @Column({ type: 'varchar', nullable: false, unique: true })
+  email: string
+
+  @Field()
+  @Column({ type: 'varchar', nullable: false, unique: true })
+  nickname: string
+
+  @Field()
+  @Column({ type: 'varchar', nullable: false })
+  password: string
+
+  @Field()
+  @Column({ type: 'varchar', nullable: true })
+  city: string
+
+  @Field()
+  @Column({ type: 'varchar', nullable: true })
+  firstName: string
+
+  @Field()
+  @Column({ type: 'varchar', nullable: true })
+  lastName: string
+
+  @Field()
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date
+
+  @Field()
+  @Column({ type: 'timestamp', nullable: true })
+  lastLogin: Date
+}
