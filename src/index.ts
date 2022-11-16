3ff8b2897a8c1d59c723221883ee44a3ad08912a
Holmes-EH <holmes.samuel@yahoo.fr>
2022-11-16 14:25:54 +0100

    add userResolver to apolloServer

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,22 @@
 import 'reflect-metadata'
 import { ApolloServer } from 'apollo-server'
+import { buildSchema } from 'type-graphql'
+import { UserResolver } from './resolvers/userResolver'
 import datasource from './utils'
 
 const port = 5000
 
-const typeDefs = `#graphql
-  type Book {
-    title: String
-    author: String
-  }
-  type Query {
-    books: [Book]
-  }
-`
-
-const books = [
-    {
-        title: 'The Awakening',
-        author: 'Kate Chopin',
-    },
-    {
-        title: 'City of Glass',
-        author: 'Paul Auster',
-    },
-]
-const resolvers = {
-    Query: {
-        books: () => books,
-    },
-}
-
 const start = async (): Promise<void> => {
-    await datasource.initialize()
-    const server = new ApolloServer({
-        typeDefs,
-        resolvers,
-    })
+  await datasource.initialize()
+  const schema = await buildSchema({ resolvers: [UserResolver] })
+  const server = new ApolloServer({ schema })
 
-    try {
-        const { url }: { url: string } = await server.listen({ port })
-        console.log(`🚀  Server ready at ${url}`)
-    } catch (error) {
-        console.error('Error starting the server')
-    }
+  try {
+    const { url }: { url: string } = await server.listen({ port })
+    console.log(`🚀  Server ready at ${url}`)
+  } catch (error) {
+    console.error('Error starting the server')
+  }
 }
 
 void start()
