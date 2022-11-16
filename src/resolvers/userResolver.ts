03dd753a822f8560f5c6aed3fef12d3426cd4b33
Holmes-EH <holmes.samuel@yahoo.fr>
2022-11-16 16:31:59 +0100

    make optional fields nullable on create

--- src/resolvers/userResolver.ts
+++ src/resolvers/userResolver.ts
@@ -14,12 +14,23 @@ export class UserResolver {
   async createUser(
     @Arg('email') email: string,
     @Arg('password') password: string,
-    @Arg('nickname') nickname: string
+    @Arg('nickname') nickname: string,
+    @Arg('city', { nullable: true }) city: string,
+    @Arg('description', { nullable: true }) description: string,
+    @Arg('avatar', { nullable: true }) avatar: string,
+    @Arg('firstName', { nullable: true }) firstName: string,
+    @Arg('lastName', { nullable: true }) lastName: string
   ): Promise<User> {
     const newUser = new User()
     newUser.email = email
     newUser.nickname = nickname
     newUser.password = await argon2.hash(password)
+    newUser.city = city
+    newUser.firstName = firstName
+    newUser.lastName = lastName
+    newUser.description = description
+    newUser.avatar = avatar
+    newUser.lastLogin = new Date()
 
     const userFromDb = await dataSource.manager.save(User, newUser)
     return userFromDb
