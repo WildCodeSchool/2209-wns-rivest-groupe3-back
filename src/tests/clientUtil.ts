deb749e9b90dd42ab11ffe330d0eabb2364d2719
Samuel Holmes <samuel.holmes@origins-digital.com>
2023-01-19 20:14:44 +0100

    :sparkles: create setup utility function for tests

--- src/tests/clientUtil.ts
+++ src/tests/clientUtil.ts
@@ -3,7 +3,7 @@ import fetch from 'cross-fetch'
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'http://localhost:5000/',
+    uri: 'http://localhost:5555/',
     fetch,
   }),
   cache: new InMemoryCache(),
