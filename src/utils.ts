4a100506757d90969d2a97c144e13ae308bbde73
HolmesEH <s.holmes.eh@gmail.com>
2023-03-29 16:04:43 +0200

    :card_file_box: use env vars for db connection

--- src/utils.ts
+++ src/utils.ts
@@ -4,15 +4,21 @@ import { DataSource } from 'typeorm'
 
 dotenv.config()
 
-const dbHost = process.env.NODE_ENV === 'test' ? 'db-test' : 'db'
-const dbPort = process.env.NODE_ENV === 'test' ? 5433 : 5432
+const username = process.env.PGUSER
+const password =
+  process.env.NODE_ENV !== 'production'
+    ? 'example'
+    : process.env.POSTGRES_PASSWORD
+
+const host = process.env.NODE_ENV === 'test' ? 'db-test' : 'db'
+const port = process.env.NODE_ENV === 'test' ? 5433 : 5432
 
 const dataSource = new DataSource({
   type: 'postgres',
-  host: dbHost,
-  port: dbPort,
-  username: 'postgres',
-  password: 'example',
+  host,
+  port,
+  username,
+  password,
   database: 'postgres',
   synchronize: false,
   entities: [path.join(__dirname, './entities/*.ts')],
