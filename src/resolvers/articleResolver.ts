7cc79aef3f6cfce4b744b6904e4761a8d316a698
HolmesEH <s.holmes.eh@gmail.com>
2023-05-23 17:00:26 +0200

    :bug: fix versioning of article content

--- src/resolvers/articleResolver.ts
+++ src/resolvers/articleResolver.ts
@@ -95,6 +95,13 @@ class DeleteArticleArgs {
   blogId: string
 }
 
+interface IContext {
+  userFromToken?: {
+    userId: string
+    email: string
+  }
+}
+
 @Resolver(Article)
 export class ArticleResolver {
   @Authorized()
@@ -163,10 +170,9 @@ export class ArticleResolver {
 
   @Query(() => Article)
   async getOneArticle(
-    @Ctx() context: { userFromToken: { userId: string; email: string } },
+    @Ctx() context: IContext,
     @Arg('slug') slug: string,
     @Arg('blogSlug') blogSlug: string,
-    @Arg('version', { nullable: true }) version?: number,
     @Arg('allVersions', { nullable: true }) allVersions?: boolean,
     @Arg('current', { nullable: true }) current?: boolean
   ): Promise<Article> {
@@ -175,33 +181,26 @@ export class ArticleResolver {
         relations: { user: true },
         where: { slug: blogSlug },
       })
-      if (version !== undefined) {
-        return await dataSource.manager.findOneOrFail(Article, {
-          relations: { articleContent: true },
-          where: {
-            articleContent: { version },
-            slug,
-            blog: { id: blog.id },
-            show: true,
-          },
-        })
-      }
+      // Early exit if no user or user isn't owner of blog
       if (
-        context.userFromToken !== undefined &&
-        blog.user.id === context.userFromToken.userId
+        context?.userFromToken === undefined ||
+        context.userFromToken.userId !== blog.user.id
       ) {
         return await dataSource.manager.findOneOrFail(Article, {
           relations: { articleContent: true },
           where: {
             slug,
             blog: { id: blog.id },
+            show: true,
+            articleContent: { current: true },
           },
         })
       }
-      if (allVersions ?? false) {
+
+      if (allVersions !== undefined && allVersions) {
         return await dataSource.manager.findOneOrFail(Article, {
           relations: { articleContent: true },
-          where: { slug, blog: { id: blog.id }, show: true },
+          where: { slug, blog: { id: blog.id } },
           order: { articleContent: { version: 'asc' } },
         })
       }
@@ -265,7 +264,7 @@ export class ArticleResolver {
   @Authorized()
   @Mutation(() => Article)
   async updateArticle(
-    @Ctx() context: { userFromToken: { userId: string; email: string } },
+    @Ctx() context: IContext,
     @Args()
     {
       articleId,
@@ -279,6 +278,8 @@ export class ArticleResolver {
     }: UpdateArticleArgs
   ): Promise<Article> {
     try {
+      if (context?.userFromToken === undefined)
+        throw new Error('You are not authorized to update this article=.')
       const {
         userFromToken: { userId },
       } = context
@@ -289,7 +290,7 @@ export class ArticleResolver {
         },
       })
       if (blog.user.id !== userId) {
-        throw new Error('You are not authorized to update this blog..')
+        throw new Error('You are not authorized to update this article.')
       }
       const article = await dataSource.manager.findOneOrFail(Article, {
         where: { id: articleId },
@@ -309,11 +310,12 @@ export class ArticleResolver {
 
       // If incoming version is different than db, then article.content has been updated
       if (article.version !== version) {
-        // start by setting previous content.current to false
-        await dataSource.manager.update(
-          Content,
-          { id: article.articleContent[article.articleContent.length - 1].id },
-          { current: false }
+        // start by setting all previous content.current to false
+        await dataSource.manager.save(
+          article.articleContent.map((existingContent) => {
+            existingContent.current = false
+            return existingContent
+          })
         )
 
         const newContent = new Content()
